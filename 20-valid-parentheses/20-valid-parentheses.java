dcd71c47180da05ca7d5ec55f09ae7d050656dd3
HarshxiT <75724680+HarshxiT@users.noreply.github.com>
2022-02-02 15:51:06 +0530

    Time: 1 ms (99.30%), Space: 42.4 MB (5.15%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,7 +1,11 @@
 class Solution {
     public boolean isValid(String s) {
         LinkedList<Character> st=new LinkedList<>();
+        // int r=0;
+        // int sq=0;
+        // int c=0;
         for(char ch:s.toCharArray()){
+
             if(ch=='(' || ch=='[' || ch=='{')st.addLast(ch);
             else if(st.size()==0)return false;
             else if(st.getLast()=='(' && ch==')')st.removeLast();
