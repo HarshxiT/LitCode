a850b8b4ce6654d50527edc3ee502d459801a48a
HarshxiT <75724680+HarshxiT@users.noreply.github.com>
2022-02-24 13:22:15 +0530

    Time: 5 ms (48.63%), Space: 51.7 MB (22.81%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int candy(int[] ratings) {
+        int[] candy = new int[ratings.length];
+        Arrays.fill(candy, 1);
+        for (int i = 1; i < candy.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candy[i] = candy[i - 1] + 1;
+            }
+        }
+        int s = candy[candy.length - 1];
+        for (int i = candy.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candy[i] = Math.max(candy[i], candy[i + 1] + 1);
+            }
+            s += candy[i];
+        }
+        return s;
+    }
+}
