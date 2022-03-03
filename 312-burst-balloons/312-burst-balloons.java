515c09fb9b56105ca0df0944d1dc1e6dea8f46a9
HarshxiT <75724680+HarshxiT@users.noreply.github.com>
2022-03-03 21:05:42 +0530

    Time: 90 ms (81.57%), Space: 43.5 MB (5.81%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.java
+++ 312-burst-balloons/312-burst-balloons.java
@@ -1,18 +1,19 @@
 class Solution {
+
     public int maxCoins(int[] nums) {
-        int[][] dp=new int[nums.length][nums.length];
-        for(int g=0;g<nums.length;g++){
-            for(int i=0,j=g; j<nums.length;j++,i++){
-                 int max=Integer.MIN_VALUE;
-                for(int k=i;k<=j;k++){
-                    int left=(k==i?0:dp[i][k-1]);
-                    int right=(k==j?0:dp[k+1][j]);
-                    int val=(i==0?1:nums[i-1])*nums[k]*(j==nums.length-1?1:nums[j+1]);
-                    max=Math.max(val+left+right,max);
+        int[][] dp = new int[nums.length][nums.length];
+        for (int g = 0; g < nums.length; g++) {
+            for (int i = 0, j = g; j < nums.length; j++, i++) {
+                int max = Integer.MIN_VALUE;
+                for (int k = i; k <= j; k++) {
+                    int left = (k == i ? 0 : dp[i][k - 1]);
+                    int right = (k == j ? 0 : dp[k + 1][j]);
+                    int val = (i == 0 ? 1 : nums[i - 1]) * nums[k] * (j == nums.length - 1 ? 1 : nums[j + 1]);
+                    max = Math.max(val + left + right, max);
                 }
-                dp[i][j]=max;
+                dp[i][j] = max;
             }
         }
-        return dp[0][nums.length-1];
+        return dp[0][nums.length - 1];
     }
 }
